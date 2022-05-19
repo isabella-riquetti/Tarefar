ee75f4ff93f0263cffaa08ef8b0c5a786a84f7ee
Isabella Riquetti <riquetti.isabella@gmail.com>
2022-05-19 15:28:30 -0300

    Test auth helper

---
+++ Tarefar.Tests/Services/Helpers/AuthenticationHelperTest.cs
@@ -0,0 +1,64 @@
+﻿using NSubstitute;
+using Xunit;
+using Tarefar.DB.Models;
+using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Configuration;
+using Tarefar.Services.Helpers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using FluentAssertions;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Tarefar.Tests.Services.Helpers
+{    public class AuthenticationHelperTest
+    {
+        #region GetToken
+        [Theory, MemberData(nameof(GetTokenTests))]
+        public void GetTokenTest(GetTokenTestInput test)
+        {
+            var configurationSubstitute = Substitute.For<IConfiguration>();
+            configurationSubstitute["JWT:ValidIssuer"] = "http://localhost:5000";
+            configurationSubstitute["JWT:ValidAudience"] = "http://localhost:4200";
+            configurationSubstitute["JWT:Secret"] = "JWTAuthenticationHIGHsecuredPasswordTESTTESTTEST";
+
+            JwtSecurityToken response = AuthenticationHelper.GetToken(test.Claims, configurationSubstitute);
+
+            response.Should().BeEquivalentTo(test.GetExpectedResponse());
+        }
+
+        public static readonly TheoryData<GetTokenTestInput> GetTokenTests = new()
+        {
+            new GetTokenTestInput()
+            {
+                TestName = "Get token",
+                ExpectSuccess = true,
+                Claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "isabella"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role, UserRoles.Default)
+                }
+            }
+        };
+        public class GetTokenTestInput
+        {
+            public string TestName { get; set; }
+            public bool ExpectSuccess { get; set; }
+            public List<Claim> Claims { get; set; }
+
+            public JwtSecurityToken GetExpectedResponse()
+            {
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("JWTAuthenticationHIGHsecuredPasswordTESTTESTTEST"));
+                return new JwtSecurityToken(
+                    issuer: "http://localhost:5000",
+                    audience: "http://localhost:4200",
+                    expires: DateTime.Now.Date.AddDays(7),
+                    claims: Claims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
+            }
+        }
+        #endregion GetToken
+    }
+}
